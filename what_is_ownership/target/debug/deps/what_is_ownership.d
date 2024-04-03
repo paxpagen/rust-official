e028ed490832242b3f2f56120186296ab10b5bff
paxpagen <drakedogwarlock@gmail.com>
2024-04-03 19:28:06 +0300

    4.1 What is Ownership?

---
+++ what_is_ownership/target/debug/deps/what_is_ownership.d
@@ -0,0 +1,5 @@
+C:\rust-project\what_is_ownership\target\debug\deps\what_is_ownership.exe: src\main.rs
+
+C:\rust-project\what_is_ownership\target\debug\deps\what_is_ownership.d: src\main.rs
+
+src\main.rs:
---
+++ what_is_ownership/target/debug/what_is_ownership.d
@@ -0,0 +1,1 @@
+C:\rust-project\what_is_ownership\target\debug\what_is_ownership.exe: C:\rust-project\what_is_ownership\src\main.rs
